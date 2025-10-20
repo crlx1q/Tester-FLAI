849dd91ea62eabad37d87f809aefe18f7ba79ad7
crlx1q <aliser.sagadi555@gmail.com>
2025-10-20 23:22:31 +0500

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,6 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
-const bcrypt = require('bcryptjs');
 const Database = require('../utils/database-mongo');
 
 const router = express.Router();
